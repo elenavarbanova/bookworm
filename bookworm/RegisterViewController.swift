cb18edd734549e116693d48cb3f6b9863247424c
Elena Varbanova <elena.varbanova04@gmail.com>
2023-01-07 00:43:04 +0200

    Validation for text fields in registartion

--- bookworm/RegisterViewController.swift
+++ bookworm/RegisterViewController.swift
@@ -11,19 +11,63 @@ class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    func validateTextFields() -> String? {
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            usernameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        {
+            return "All fields are required!"
+        }
+        
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if checkPassword(cleanedPassword) == false {
+            return "The password does not meet the password policy requirements!"
+        }
+        
+        let cleanedEmail = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if checkEmail(cleanedEmail) == false {
+            return "The email does not meet the email policy requirements!"
+        }
+        
+        return nil
     }
-    */
-
+    
+    private func checkPassword(_ password: String) -> Bool {
+        let passwordRegEx = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[A-Za-z])(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}")
+        return passwordRegEx.evaluate(with: password)
+    }
+    
+    private func checkEmail(_ email: String) -> Bool {
+        let emailRegEx = NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")
+        return emailRegEx.evaluate(with: email)
+    }
+    
+    func showError(_ errorMessage: String) {
+        errorLabel.text = errorMessage
+        errorLabel.alpha = 1
+    }
+    
+    @IBAction func signUpTappedButton(_ sender: Any) {
+        
+        let error = validateTextFields()
+        if error != nil {
+            showError(error!)
+        } else {
+            performSegue(withIdentifier: "signInSegue", sender: nil)
+           //create user and segue to sign in
+        }
+        
+    }
 }
--- bookworm/SignInViewController.swift
+++ bookworm/SignInViewController.swift
@@ -11,19 +11,13 @@ class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBAction func signInTappedButton(_ sender: Any) {
+        
     }
-    */
-
 }
