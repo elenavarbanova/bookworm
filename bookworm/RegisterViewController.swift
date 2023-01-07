05647b4862f4f997faab572cc8673f7f35da252a
Elena Varbanova <elena.varbanova04@gmail.com>
2023-01-08 01:24:45 +0200

    Remove adjusting for keyboard

--- bookworm/RegisterViewController.swift
+++ bookworm/RegisterViewController.swift
@@ -11,30 +11,7 @@ class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
     }
-    
-    private func setup() {
-        setupDismissKeyboardGesture()
-        setupKeyboard()
-    }
-    
-    private func setupKeyboard() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    private func setupDismissKeyboardGesture() {
-        let dismissKeyboardTap = UITapGestureRecognizer(target: self, action: #selector(viewTapped(_: )))
-        view.addGestureRecognizer(dismissKeyboardTap)
-    }
-    
-    @objc func viewTapped(_ recognizer: UITapGestureRecognizer) {
-        if recognizer.state == UIGestureRecognizer.State.ended {
-            view.endEditing(true) // resign first responder
-        }
-    }
-    
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -93,31 +70,5 @@ class RegisterViewController: UIViewController {
             performSegue(withIdentifier: "signInSegue", sender: nil)
         }
         
-    }
-}
-
-
-extension RegisterViewController {
-    @objc func keyboardWillShow(_ sender: NSNotification) {
-        guard let userInfo = sender.userInfo,
-                let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
-              let currentTextField = UIResponder.currentFirst() as? UITextField
-        else {
-            return
-        }
-        
-        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
-        let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
-        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
-        
-        if textFieldBottomY > keyboardTopY {
-            let textBoxY = convertedTextFieldFrame.origin.y
-            let newFrameY = (textBoxY - keyboardTopY / 2) * -1
-            view.frame.origin.y = newFrameY
-        }
-    }
-    
-    @objc func keyboardWillHide(_ sender: NSNotification) {
-        view.frame.origin.y = 0
     }
 }
