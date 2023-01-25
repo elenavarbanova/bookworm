eae2058b802e9533a732737b0a11af2416b6e0e8
Elena Varbanova <elena.varbanova04@gmail.com>
2023-01-25 21:43:42 +0200

    Log out and persistent log in

---
+++ bookworm/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  bookworm
+//
+//  Created by Elena Varbanova on 25.01.23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signOutTappedButton(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch {
+            print(error)
+        }
+        
+    
+    }
+}
